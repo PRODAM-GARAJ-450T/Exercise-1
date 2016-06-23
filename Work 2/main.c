41f37c595d24b2e5c8daa1406b8919783e9856c8
PRODAM-GARAJ-450T <saryman1972@gmail.com>
2016-06-24 01:07:15 +0300

    PEREMENNblE

--- Work 2/main.c
+++ Work 2/main.c
@@ -2,5 +2,10 @@
 #include <math.h>
 int main ()
 {
+	double a, b, c, d, x1, x2, x;
+	printf ( "BBEDITE TRI CELbIX 4ISLA \n" );
+	scanf ( "%f", &a );
+	scanf ( "%f", &b );
+	scanf ( "%f", &c );
 	return 0;
 }
